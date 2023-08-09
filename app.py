03baf359dd546d465b7d8cc3ae3b537db9a9bac1
Degaro <60842166+Degaro@users.noreply.github.com>
2023-08-09 19:15:15 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # creates a flask application
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # route to a website incl path following the home
 @app.route('/')
 def hello_world():
-  return "Hello, Steven"
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
